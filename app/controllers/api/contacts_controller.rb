7079748ca53b6108c4669802478198ec6be06b53
Luis Arturo Topete Renteria <arturotopete31@gmail.com>
2023-01-05 13:50:03 -0700

    controlls for contacts done

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,16 +1,43 @@
-class Api::ContactsController < ApplicationController
+class Api::ContactsController < ApplicationControllerclass
+  before_action :set_contact, only: [:show, :update, :destroy]
+  
   def index
+    render json: Contact.all
   end
 
   def show
+    render json: @contact
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      render json: @contact
+    else
+      render json: { errors: @contact.errors }, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @contact.update(contact_params)
+      render json: @contact
+    else
+      render json: { errors: @contact.errors }, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @contact.destroy
+    render json: { message: 'Contact deleted' }
   end
+
+  private
+
+    def contact_params
+      params.require(:contact).permit(:fname, :lname, :phone, :email)
+    end
+
+    def set_contact
+      @contact = Contact.find(params[:id])
+    end
 end
